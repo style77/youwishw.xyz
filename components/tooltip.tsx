312eaeda01186ac210269b5ef53fdaec2c927780
style77 <stylek777@gmail.com>
2022-10-01 22:10:27 +0200

    little gramar mistake

--- components/tooltip.tsx
+++ components/tooltip.tsx
@@ -7,8 +7,8 @@ export const Tooltip = ({
     return (
         <div className="relative flex flex-col items-center group">
             {children}
-            <div className="absolute -bottom-7 flex flex-col items-center mb-6 group-hover:opacity-50 opacity-0 transition">
-                <span className="absolute w-32 h-12 z-10 p-2 text-xs text-white whitespace-no-wrap bg-gray-900 shadow-lg rounded-md">{message}</span>
+            <div className="absolute -bottom-7 flex flex-col items-center mb-6 group-hover:opacity-100 opacity-0 transition">
+                <span className="absolute w-32 h-16 z-10 p-2 text-xs text-white whitespace-no-wrap bg-gray-900 shadow-lg rounded-md">{message}</span>
                 <div className="w-3 h-3 -mt-2 rotate-45 bg-gray-900"></div>
             </div>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -34,26 +34,26 @@ const Home: NextPage = () => {
       <div className="select-none">
         <div className="cursor-default duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out" data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>
           <span className="text-6xl text-slate-100 font-light antialiased block mb-2 transition ease-in-out duration-300 hover:text-slate-100">youwish</span>
-          <span className="text-xl text-slate-300 font-light antialiased transition ease-in-out duration-300 hover:text-slate-200">Developer for your needs</span>
+          <span className="text-xl text-slate-300 font-light antialiased transition ease-in-out duration-300 hover:text-slate-200">a developer for your needs</span>
         </div>
-        <div className="inline-grid grid-cols-3 gap-5 mt-5 inline">
+        <div className="inline-grid grid-cols-3 gap-6 mt-5 inline">
           <div id="github" className="delay-300 duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out inline" data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>
             <a href={`https://github.com/${GITHUB}`}>
-              <div className="h-14 w-14 rounded-full bg-slate-400 transition ease-in-out duration-300 hover:bg-slate-100 grid place-items-center">
+              <div className="h-14 w-14 rounded-full bg-gray-900 transition ease-in-out duration-300 hover:bg-slate-100 grid place-items-center">
                 <FiGithub className="h-7 w-7 block m-auto" />
               </div>
             </a>
           </div>
           <div id="contact" className="delay-[600ms] duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out inline" data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>
             <a href={`mailto:${MAIL}`}>
-              <div className="h-14 w-14 rounded-full bg-slate-400 transition ease-in-out duration-300 hover:bg-slate-100 grid place-items-center">
+              <div className="h-14 w-14 rounded-full bg-gray-900 transition ease-in-out duration-300 hover:bg-slate-100 grid place-items-center">
                 <FiMail className="h-7 w-7 block m-auto" />
               </div>
             </a>
           </div>
           <div id="TODO" className="delay-[900ms] duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out inline" data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>
-            <Tooltip message="apps should be always scallable ig">
-              <div className="h-14 w-14 rounded-full bg-slate-400 transition ease-in-out duration-300 grid place-items-center">
+            <Tooltip message="TBA (apps should be always scallable anyway ig)">
+              <div className="h-14 w-14 rounded-full bg-gray-900 transition ease-in-out duration-300 grid place-items-center">
                 <FiX className="h-7 w-7 block m-auto" />
               </div>
             </Tooltip>
