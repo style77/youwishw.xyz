ff38dcaafec35f71f90a413b34bb275f138043b8
style77 <stylek777@gmail.com>
2022-10-02 01:24:53 +0200

    added seo and better design

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,10 @@ export default function Document() {
         <Html lang='en'>
             <Head>
                 <title>youwish.dev</title>
+                <meta name="robots" content="all" />
             </Head>
             <body>
-                <div id="background-fade" className="w-screen bg-black/75">
+                <div id="background-fade" className="w-screen backdrop-blur-lg" style={{ background: '-webkit-radial-gradient(rgb(255,255,255,0.1) 5%, rgb(0,0,0,1))' }}>
                     <section id="stars" className="text-center align-center justify-center">
                         <span></span>
                         <span></span>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import type { NextPage } from 'next'
 import React, { useEffect, useState } from "react"
 
 import { FiGithub, FiMail, FiX } from 'react-icons/fi'
-import { Tooltip } from '../components/tooltip';
+import { Tooltip } from '../components/tooltip'
+
+import { NextSeo } from 'next-seo'
 
 const MAIL = "stylek777@gmail.com"
 const GITHUB = "Style77"
@@ -30,37 +32,49 @@ const Home: NextPage = () => {
   }, [])
 
   return (
-    <div className="grid h-screen place-items-center z-index-10">
-      <div className="select-none">
-        <div className="cursor-default duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out" data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>
-          <span className="text-6xl text-slate-100 font-light antialiased block mb-2 transition ease-in-out duration-300 hover:text-slate-100">youwish</span>
-          <span className="text-xl text-slate-300 font-light antialiased transition ease-in-out duration-300 hover:text-slate-200">a developer for your needs</span>
-        </div>
-        <div className="inline-grid grid-cols-3 gap-6 mt-5 inline">
-          <div id="github" className="delay-300 duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out inline" data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>
-            <a href={`https://github.com/${GITHUB}`}>
-              <div className="h-14 w-14 rounded-full bg-gray-900 transition ease-in-out duration-300 hover:bg-slate-100 grid place-items-center">
-                <FiGithub className="h-7 w-7 block m-auto" />
-              </div>
-            </a>
+    <>
+      <NextSeo title="youwishw - a developer for your needs"
+        description='Developer for your needs - Python/Javascript/TypeScript/Golang/React/Next and many more!'
+        canonical="https://www.canonical.ie/"
+        openGraph={{
+          url: 'https://',
+          title: 'youwish - a developer for your needs',
+          description: 'Developer for your needs - Python/Javascript/TypeScript/Golang/React/Next and many more!',
+          site_name: 'youwishw.xyz',
+        }}
+      />
+      <div className="grid h-screen place-items-center z-index-10">
+        <div className="select-none">
+          <div className="cursor-default duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out" data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>
+            <span className="text-6xl text-slate-100 font-light antialiased block mb-2 transition ease-in-out duration-300 hover:text-slate-100" id="title">youwish</span>
+            <span className="text-xl text-slate-300 font-light antialiased transition ease-in-out duration-300 hover:text-slate-200" id="subtitle">a developer for your needs</span>
           </div>
-          <div id="contact" className="delay-[600ms] duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out inline" data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>
-            <a href={`mailto:${MAIL}`}>
-              <div className="h-14 w-14 rounded-full bg-gray-900 transition ease-in-out duration-300 hover:bg-slate-100 grid place-items-center">
-                <FiMail className="h-7 w-7 block m-auto" />
-              </div>
-            </a>
-          </div>
-          <div id="TODO" className="delay-[900ms] duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out inline" data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>
-            <Tooltip message="TBA (apps should be always scallable anyway ig)">
-              <div className="h-14 w-14 rounded-full bg-gray-900 transition ease-in-out duration-300 grid place-items-center">
-                <FiX className="h-7 w-7 block m-auto" />
-              </div>
-            </Tooltip>
+          <div className="inline-grid grid-cols-3 gap-6 mt-5 inline z-index-10">
+            <div id="github" className="delay-300 duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out inline" data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>
+              <a href={`https://github.com/${GITHUB}`}>
+                <div className="h-14 w-14 rounded-full bg-violet-900 text-slate-200 transition ease-in-out duration-300 hover:bg-slate-100 hover:text-zinc-900 grid place-items-center">
+                  <FiGithub className="h-7 w-7 block m-auto" />
+                </div>
+              </a>
+            </div>
+            <div id="contact" className="delay-[600ms] duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out inline" data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>
+              <a href={`mailto:${MAIL}`}>
+                <div className="h-14 w-14 rounded-full bg-violet-900 text-slate-200 transition ease-in-out duration-300 hover:bg-slate-100 hover:text-zinc-900 grid place-items-center">
+                  <FiMail className="h-7 w-7 block m-auto" />
+                </div>
+              </a>
+            </div>
+            <div id="TODO" className="delay-[900ms] duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out inline" data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>
+              <Tooltip message="TBA (apps should be always scallable anyway ig)">
+                <div className="h-14 w-14 rounded-full bg-violet-900 text-slate-200 transition ease-in-out duration-300 grid place-items-center">
+                  <FiX className="h-7 w-7 block m-auto" />
+                </div>
+              </Tooltip>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
