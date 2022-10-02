5715754cb269005ee77947a1e324264eedccaf19
style77 <stylek777@gmail.com>
2022-10-02 02:03:12 +0200

    fixing seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,10 +34,10 @@ const Home: NextPage = () => {
   return (
     <>
       <NextSeo title="youwish - a developer for your needs"
-        description='Developer for your needs - Python/Javascript/TypeScript/Golang/React/Next and many more!'
+        description='Developer for your needs - Python/Javascript/TypeScript/Golang/React/Next and many more! Affordable prices, big experience and fast learner in case of compulsion to use new technology. Check right now!'
         canonical="https://youwishw.xyz/"
         openGraph={{
-          url: 'https://',
+          url: 'https://youwishw.xyz/',
           title: 'youwish - a developer for your needs',
           description: 'Developer for your needs - Python/Javascript/TypeScript/Golang/React/Next and many more!',
           site_name: 'youwishw.xyz',
@@ -47,7 +47,7 @@ const Home: NextPage = () => {
         <div className="select-none">
           <div className="cursor-default duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out" data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>
             <h1 className="text-6xl text-slate-100 font-light antialiased block mb-2 transition ease-in-out duration-300 hover:text-slate-100" id="title">youwish </h1>
-            <h4 className="text-xl text-slate-300 font-light antialiased transition ease-in-out duration-300 hover:text-slate-200" id="subtitle">a developer for your needs</h4>
+            <h1 className="text-xl text-slate-300 font-light antialiased transition ease-in-out duration-300 hover:text-slate-200" id="subtitle">a developer for your needs</h1>
           </div>
           <div className="inline-grid grid-cols-3 gap-6 mt-5 inline z-index-10">
             <div id="github" className="delay-300 duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out inline" data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>
