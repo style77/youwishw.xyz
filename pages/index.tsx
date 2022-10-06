52b502f35812dbe5f00e858a60c8e3349325cb50
style77 <stylek777@gmail.com>
2022-10-06 16:36:00 +0200

    added darker background to make text more readable

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ const Home: NextPage = () => {
     const backgroundNum = () =>
       Math.floor(Math.random() * BACKGROUNDS_COUNT) + 1;
 
-    let interval: any
+    let interval: any;
 
     const timer = () => {
       document.body.style.background =
@@ -42,6 +42,7 @@ const Home: NextPage = () => {
         backgroundNum() +
         ".jpg') no-repeat center center fixed";
 
+      document.body.style.backgroundSize = "100% 100%";
       document.body.style.transition = "background 2s";
 
       interval = setTimeout(timer, 20000);
@@ -123,58 +124,63 @@ const Home: NextPage = () => {
           site_name: "youwishw.xyz",
         }}
       />
-      <div className="grid h-screen place-items-center z-index-10">
+      <div className="grid h-screen place-items-center justify-center items-center z-index-10">
         <div className="select-none">
           <div
-            className="cursor-default duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out"
+            className="p-4 w-[23rem] md:w-[40rem] bg-zinc-900/30 rounded-2xl duration-700 transform opacity-0 transition-all translate-y-12 ease-out"
             data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'
           >
-            <h1
-              className="text-6xl text-slate-100 font-light antialiased block mb-2 transition ease-in-out duration-300 hover:text-slate-100"
-              id="title"
-            >
-              youwish{" "}
-            </h1>
-            <h1
-              className="text-xl text-slate-300 font-light antialiased transition ease-in-out duration-300 hover:text-slate-200"
-              id="subtitle"
-            >
-              a developer for your needs
-            </h1>
-          </div>
-          <div className="inline-grid grid-cols-3 gap-6 mt-5 z-index-10">
             <div
-              id="github"
-              className="delay-300 duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out inline"
+              className="cursor-default duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out"
               data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'
             >
-              <a href={`https://github.com/${GITHUB}`}>
-                <div className="h-14 w-14 rounded-full bg-zinc-900 text-slate-200 transition ease-in-out duration-300 hover:bg-slate-100 hover:text-zinc-900 grid place-items-center">
-                  <FiGithub className="h-7 w-7 block m-auto" />
-                </div>
-              </a>
+              <h1
+                className="text-6xl text-slate-100 font-light antialiased block mb-2 transition ease-in-out duration-300 hover:text-slate-100"
+                id="title"
+              >
+                youwish{" "}
+              </h1>
+              <h1
+                className="text-xl text-slate-300 font-light antialiased transition ease-in-out duration-300 hover:text-slate-200"
+                id="subtitle"
+              >
+                a developer for your needs
+              </h1>
             </div>
-            <div
-              id="contact"
-              className="delay-[600ms] duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out inline"
-              data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'
-            >
-              <a href={`mailto:${MAIL}`}>
-                <div className="h-14 w-14 rounded-full bg-zinc-900 text-slate-200 transition ease-in-out duration-300 hover:bg-slate-100 hover:text-zinc-900 grid place-items-center">
-                  <FiMail className="h-7 w-7 block m-auto" />
-                </div>
-              </a>
-            </div>
-            <div
-              id="tg-discord"
-              className="delay-[900ms] duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out inline"
-              data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'
-            >
-              <a href="https://t.me/youwishw">
-                <div className="h-14 w-14 rounded-full bg-zinc-900 text-slate-200 transition ease-in-out duration-300 hover:bg-slate-100 hover:text-zinc-900 grid place-items-center">
-                  <FaTelegramPlane className="w-7 h-7" />
-                </div>
-              </a>
+            <div className="inline-grid grid-cols-3 gap-6 mt-5 z-index-10">
+              <div
+                id="github"
+                className="delay-300 duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out inline"
+                data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'
+              >
+                <a href={`https://github.com/${GITHUB}`}>
+                  <div className="h-14 w-14 rounded-full bg-zinc-900 text-slate-200 transition ease-in-out duration-300 hover:bg-slate-100 hover:text-zinc-900 grid place-items-center">
+                    <FiGithub className="h-7 w-7 block m-auto" />
+                  </div>
+                </a>
+              </div>
+              <div
+                id="contact"
+                className="delay-[600ms] duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out inline"
+                data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'
+              >
+                <a href={`mailto:${MAIL}`}>
+                  <div className="h-14 w-14 rounded-full bg-zinc-900 text-slate-200 transition ease-in-out duration-300 hover:bg-slate-100 hover:text-zinc-900 grid place-items-center">
+                    <FiMail className="h-7 w-7 block m-auto" />
+                  </div>
+                </a>
+              </div>
+              <div
+                id="tg-discord"
+                className="delay-[900ms] duration-700 relative transform opacity-0 transition-all translate-y-12 ease-out inline"
+                data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'
+              >
+                <a href="https://t.me/youwishw">
+                  <div className="h-14 w-14 rounded-full bg-zinc-900 text-slate-200 transition ease-in-out duration-300 hover:bg-slate-100 hover:text-zinc-900 grid place-items-center">
+                    <FaTelegramPlane className="w-7 h-7" />
+                  </div>
+                </a>
+              </div>
             </div>
           </div>
         </div>
